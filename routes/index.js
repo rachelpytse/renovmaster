7a6fd7e494f1b1ce5d06d73c3a006a6588e0a8e5
rach <rachelpytse@gmail.com>
2023-08-18 17:43:25 +0200

    added profile-edit.hbs and added route to edit profile

--- routes/index.js
+++ routes/index.js
@@ -75,11 +75,89 @@ router.post("/login", (req, res, next) => {
 /* Profile Page */
 router.get("/profile", (req, res, next) => {
   if (req.session.currentUser) {
-    res.render('profile', {userInSession : req.session.currentUser})
-    }
-  else {
+    // Fetch the most up-to-date user data from the database
+    User.findById(req.session.currentUser._id)
+      .then((userData) => {
+        res.render('profile', { userInSession: userData });
+      })
+      .catch((error) => {
+        console.error("Error while getting the user from db", error);
+        next(error);
+      });
+  } else {
     res.redirect("/login");
   }
 });
+
+
+// Edit Profile
+router.get("/profile/edit", (req, res, next) => {
+  if (!req.session.currentUser) {
+    return res.redirect("/login");
+  }
+  
+  User.findById(req.session.currentUser._id)
+    .then((userData) => {
+      res.render("profile-edit", { userInSession: userData });
+    })
+    .catch((error) => {
+      console.error("Error while getting the user from db", error);
+      next(error);
+    });
+});
+
+router.post("/profile/edit", (req, res, next) => {
+  if (!req.session.currentUser) {
+    return res.redirect("/login");
+  }
+  
+  const { fullName, companyName, companyRegistrationNb, companyAddress, companyPostCode, phoneNb, email, password } = req.body;
+
+  // Check if the new email is the same as the current user's email
+  if (email === req.session.currentUser.email) {
+    // Skip checking for duplicate email and continue with the update
+  } else {
+    // Check if the new email already exists in the database (excluding the current user's document)
+    User.findOne({ email: email })
+      .then((user) => {
+        if (user) {
+          // Send an error message to the client because this email is already associated with another user
+          return res.render("profile-edit", { errorMessage: "This email is already associated with another account." });
+        } else {
+          // Continue with the update
+        }
+      })
+      .catch((error) => {
+        console.error("Error while checking for duplicate email", error);
+        next(error);
+      });
+  }
+  
+  bcryptjs
+    .genSalt(saltRounds)
+    .then((salt) => bcryptjs.hash(password, salt))
+    .then((hashedPassword) => {
+      return User.findByIdAndUpdate(req.session.currentUser._id, {
+        fullName: fullName, 
+        password: hashedPassword,
+        companyName: companyName,
+        companyRegistrationNb:companyRegistrationNb ,
+        companyAddress: companyAddress,
+        companyPostCode: companyPostCode,
+        phoneNb: phoneNb,
+        email: email,
+      }, { new: true });
+    })
+    .then(() => {
+      res.redirect("/profile");
+      req.session.currentUser = updatedUser;
+    })
+    .catch((error) => {
+      console.error("Error while updating the user", error);
+      next(error);
+    });
+});
+
+
 
 module.exports = router;
